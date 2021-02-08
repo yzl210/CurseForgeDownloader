f0f8b5d107ef2f937220d443fa96332b12d8d2b3
yzl210 <2846840578@qq.com>
2021-02-09 00:58:32 +0800

    Fix build.gradle and remove unnecessary libraries

--- src/main/java/cn/leomc/cfdownloader/MessageUtils.java
+++ src/main/java/cn/leomc/cfdownloader/MessageUtils.java
@@ -1,29 +1,58 @@
 package cn.leomc.cfdownloader;
 
 import javafx.application.Platform;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import org.controlsfx.dialog.ExceptionDialog;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class MessageUtils {
 
 
     public static void error(Throwable exception) {
         Platform.runLater(() -> {
-            ExceptionDialog dialog = new ExceptionDialog(exception);
-            dialog.setHeaderText(exception.getClass().getName());
-            dialog.showAndWait();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(exception.toString());
+            alert.setHeaderText(exception.getClass().getName());
+            alert.setContentText(exception.getLocalizedMessage());
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            exception.printStackTrace(pw);
+            String exceptionText = sw.toString();
+
+            Label label = new Label("The exception stacktrace was:");
+
+            TextArea textArea = new TextArea(exceptionText);
+            textArea.setEditable(false);
+            textArea.setWrapText(true);
+
+            textArea.setMaxWidth(Double.MAX_VALUE);
+            textArea.setMaxHeight(Double.MAX_VALUE);
+            GridPane.setVgrow(textArea, Priority.ALWAYS);
+            GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+            GridPane expContent = new GridPane();
+            expContent.setMaxWidth(Double.MAX_VALUE);
+            expContent.add(label, 0, 0);
+            expContent.add(textArea, 0, 1);
+
+            alert.getDialogPane().setExpandableContent(expContent);
+
+            alert.showAndWait();
+
         });
     }
 
     public static void info(String message) {
         Platform.runLater(() -> {
-            Dialog<ButtonType> dialog = new Dialog<>();
-            dialog.setTitle("info");
-            dialog.setHeaderText(message);
-            dialog.setContentText(message);
-            dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
-            dialog.showAndWait();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("info");
+            alert.setHeaderText(message);
+            alert.setContentText(message);
+            alert.showAndWait();
         });
     }
 
